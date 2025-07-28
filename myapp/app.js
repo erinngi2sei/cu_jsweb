7e5fc8938bda2753c3dc4a1e0695f3fa0c2a310c
hirohiro-private <hirohiro.iti@gmail.com>
2025-07-28 18:39:47 +0900

    end: mongoDBの接続に成功。

--- myapp/app.js
+++ myapp/app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var dogsRouter = require('./routes/dogs');
+var notes_from_bRouter = require('./routes/notes_from_b');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/dogs', dogsRouter);
+app.use('/notes_from_b', notes_from_bRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
